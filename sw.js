287be9c45f88d37ec1a260e4a8a8ad0e36689d73
rbiehn <130387363+rbiehn@users.noreply.github.com>
2026-03-19 09:44:27 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "tfs-publish-v40";
+var CACHE = "tfs-publish-v41";
 var urlsToCache = ["./", "./index.html", "./constants.js", "./utils.js", "./components.js", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e) {
