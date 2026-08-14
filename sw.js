0a84e0cdfdbbccce6e0586a848414346c12c19d3
rbiehn <130387363+rbiehn@users.noreply.github.com>
2026-08-14 14:02:55 -0500

    Update cache version from v51 to v52

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "tfs-publish-v51";
+var CACHE = "tfs-publish-v52";
 var urlsToCache = ["./", "./index.html", "./constants.js", "./utils.js", "./components.js", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e) {
